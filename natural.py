2dcb5c54c542cec9e79043aa0f159afa2ae7d3a7
jayanthisadam <41771096+jayanthisadam@users.noreply.github.com>
2018-08-20 20:29:40 +0530

    Create natural.py

---
+++ natural.py
@@ -0,0 +1,11 @@
+num1 = int(input(""))
+hold = num1
+sum = 0
+
+if num1 <= 0: 
+   print("invalid") 
+else: 
+   while num1 > 0:
+        sum = sum + num1
+        num1 = num1 - 1;
+        print(sum)
